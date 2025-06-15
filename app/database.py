5b2e1cff685c963cdf675891532353487403537f
Vicente Aguilera <vicente.aguilerae@gmail.com>
2025-06-15 18:41:24 -0400

    Setup for supabase

--- app/database.py
+++ app/database.py
@@ -6,13 +6,24 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
-# DB configuration
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+if os.getenv("DB_type") == "supabase":
+    # Fetch variables
+    DB_USER = os.getenv("SB_USER")
+    DB_PASSWORD = os.getenv("SB_PASSWORD")
+    DB_HOST = os.getenv("SB_HOST")
+    DB_PORT = os.getenv("SB_PORT")
+    DB_NAME = os.getenv("SB_NAME")
+
+    # Construct the SQLAlchemy connection string
+    DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode=require"
+else:
+    # DB configuration
+    DB_HOST = os.getenv("DB_HOST")
+    DB_PORT = os.getenv("DB_PORT")
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DB_NAME = os.getenv("DB_NAME")
+    DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 # Engine
 engine = create_engine(DATABASE_URL)
