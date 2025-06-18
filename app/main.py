cf517df2c06f5958fca6e0e10abeb33c8416600d
Vicente Aguilera <vicente.aguilerae@gmail.com>
2025-06-18 00:44:30 -0400

    Clean scripts from unused imports

--- app/main.py
+++ app/main.py
@@ -1,13 +1,9 @@
-from fastapi import FastAPI, Response, status, HTTPException, Depends
-from fastapi.params import Body
-from sqlalchemy.orm import Session
-from typing import List
+from fastapi import FastAPI
 
-from . import models, schemas
-from .database import engine, get_db
-from .utils import hash
+from . import models
+from .database import engine
 
-from .routers import post, user
+from .routers import post, user, auth
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -15,6 +11,7 @@ app = FastAPI()
 
 app.include_router(post.router)
 app.include_router(user.router)
+app.include_router(auth.router)
 
 
 @app.get("/")
